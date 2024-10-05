83bec9a1b73b4c94b71a15c318ad6fe995128bad
Sarah <158204095+sarahshaoss@users.noreply.github.com>
2024-10-05 17:51:28 -0400

    Update Shao_Sarah_Homework2_Part1.cpp

--- Shao_Sarah_Homework2_Part1.cpp
+++ Shao_Sarah_Homework2_Part1.cpp
@@ -2,7 +2,7 @@
 //10/01/2024
 //Homework 2 Part 1
 
-#include <iostream.h> // adds library
+#include <iostream> // adds library
 
 using namespace std; //shortcut
 
@@ -16,19 +16,17 @@ int main()
     cout << "Enter the day:\n> ";
     cin >> day;   //stores data in days
 
-    if ((month == 3 && day >=21) || (month >= 3 && month < 6) || (month == 6 && day <= 20))
-            cout << "It is now Spring!";
-    else 
-        if ((month == 6 && day >=21) || (month >= 6 && month < 9) || (month == 9 && day <= 22))
-                cout << "It is now Summer!";
-        else
-            if ((month == 9 && day >=23) || (month >= 9 && month < 12) || (month == 12 && day <= 21))
-                    cout << "It is now Fall!";
-            else
-            if ((month == 12 && day >=22) || (month < 3) || (month == 3 && day <= 20))
-                    cout << "It is now Winter!";
-        
-    return 0;
+  
+    if ((month == 3 && day >= 21) || (month >= 4 && month < 6) || (month == 6 && day <= 20)) //checks if the month is between 3 and 6 and between 3/21 and 6/20
+      cout << "It is now Spring!";
+    else if ((month == 6 && day >22) || (month > 5 && month < 9) || (month == 9 && day < 23)) //checks if the month is between 6 and 9 and between 6/22 and 9/23
+      cout << "It is now Summer!";
+    else if ((month == 9 && day >22) || (month > 8 && month < 12) || (month == 12 && day < 22)) //checks if the month is between 9 and 12 and between 9/22 to 12/22
+      cout << "It is now Fall!";
+    else if ((month == 12 && day >= 22) || (month < 4) || (month == 3 && day <= 20)) //checks if the month is between 12 and 3 and between 12/22 and 3/20
+        cout << "It is now Winter!";
+
+    return 0;//ends program
 
         /* Enter the month:
         > 9
