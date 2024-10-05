2db31296b5eb4f1d1f42aac8a5f70f6fafb8cc96
Sarah <158204095+sarahshaoss@users.noreply.github.com>
2024-10-05 16:54:00 -0400

    Update Shao_Sarah_Homework2_Part1.cpp

--- Shao_Sarah_Homework2_Part1.cpp
+++ Shao_Sarah_Homework2_Part1.cpp
@@ -2,19 +2,19 @@
 //10/01/2024
 //Homework 2 Part 1
 
-#include <iostream.h>
+#include <iostream.h> // adds library
 
-using namespace std;
+using namespace std; //shortcut
 
 int main()
 {
-    int month;
-    int day;
+    int month; //creates a variable to store data for month
+    int day;   //creates a variable to store data for days
 
     cout << "Enter the month:\n> ";
-    cin >> month;
+    cin >> month; //stores data in month
     cout << "Enter the day:\n> ";
-    cin >> day;
+    cin >> day;   //stores data in days
 
     if ((month == 3 && day >=21) || (month >= 3 && month < 6) || (month == 6 && day <= 20))
             cout << "It is now Spring!";
